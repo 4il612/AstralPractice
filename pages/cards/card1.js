d70639090c0743e2df974ac772cba01f360ee6ba
Nikita Shilin <shilin.nick@yandex.ru>
2022-07-05 16:28:57 +0300

    Fixed Load-btn added Card model
    Fixed bug, when LoadMore-btn has a event-listener-wrapper, now only button has events.
    Added card prefab, but need to make a color changement of svg-heart.

--- pages/cards/card1.js
+++ pages/cards/card1.js
@@ -1,14 +1,16 @@
 const Card1 = () => {
     return(
-        <div>
-            <h1>
-                Карточка 1
-            </h1>
-            <p>Дата добавления:</p>
-            <p>11 откября 2022 года</p>
-            <div>
-                ♥
+        <div className="CardWrapper">
+            <img className="CardImage" src="/Image.jpg"/>
+            <div className="CardBody">
+                Дата добавления:
+                <div className="CardDate">
+                    11 октября 2022 года
+                </div>
             </div>
+            <svg class="CardLike" viewBox="0 0 32 29.6" width="16px">
+                <path d="M23.6,0c-3.4,0-6.3,2.7-7.6,5.6C14.7,2.7,11.8,0,8.4,0C3.8,0,0,3.8,0,8.4c0,9.4,9.5,11.9,16,21.2c6.1-9.3,16-12.1,16-21.2C32,3.8,28.2,0,23.6,0z"/>
+            </svg>
         </div>
     )
 }
