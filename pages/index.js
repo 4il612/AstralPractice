cc4acb65ac4e873686b55ae6d3bad9fd1b269786
Nikita Shilin <shilin.nick@yandex.ru>
2022-07-05 23:49:16 +0300

    Grid-system for future cards
    Was created a grid-display wrapper of cards.
    Added some cards on homepage as example.

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,19 @@
 import Content from "../components/content"
 import ButtonLoad from "../components/button"
+import Card1 from "./cards/card1"
 
 export default function Home(){
   return(
     <div>
-      <Content/>
+      <div className="CardsWrapper">
+        <Card1/>
+        <Card1/>
+        <Card1/>
+        <Card1/>
+        <Card1/>
+        <Card1/>
+        <Card1/>
+      </div>
       <ButtonLoad/>
     </div>
   )
