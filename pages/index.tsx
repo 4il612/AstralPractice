ac96a9838d52ff15620dd1ac900b165fcc931136
Nikita Shilin <shilin.nick@yandex.ru>
2022-08-27 20:29:15 +0300

    fix: new link

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,44 +1,54 @@
-import { useEffect, useState } from "react"
-import axios from "../node_modules/axios/index"
-import Card from "../components/card"
-import LoadButton from "../components/loadbutton"
-import Loader from "../components/loader"
+import { useEffect, useState } from "react";
+import axios from "../node_modules/axios/index";
+import Card from "../components/card";
+import LoadButton from "../components/loadbutton";
+import Loader from "../components/loader";
 
-const Home = () =>{
-  const [cards, setCards] = useState([])
-  const [curLimit, setCurLimit] = useState(3)
-  const [fetching, setFetching] = useState(true)
+const Home = () => {
+  const [cards, setCards] = useState([]);
+  const [curLimit, setCurLimit] = useState(3);
+  const [fetching, setFetching] = useState(true);
 
   useEffect(() => {
-    if (fetching){
-      document.getElementById("Loader").style.display = 'block'
-      axios.get('https://jsonplaceholder.typicode.com/photos?_limit=' + curLimit)
-      .then(response => {
-        setCards(response.data)
-        setCurLimit(curLimit + 3)
-      })
-      .finally(() => {
-        setFetching(false)
-        document.getElementById("Loader").style.display = 'none'
-      })
+    if (fetching) {
+      document.getElementById("Loader").style.display = "block";
+      axios
+        .get("https://astral-app-ns2.herokuapp.com/api?limit=" + curLimit)
+        .then((response) => {
+          setCards(response.data);
+          setCurLimit(curLimit + 3);
+        })
+        .finally(() => {
+          setFetching(false);
+          document.getElementById("Loader").style.display = "none";
+        });
     }
-  }, [fetching])
-  
+  }, [fetching]);
+
   return (
     <>
-        <title>Cards</title>
-        <ul className="CardsWrapper">
-            { !!cards.length && cards.map(card => {
-                return(
-                    <Card key={card.id} url={card.url} alt={card.title} id={card.id}/>
-                )
-            })}
-        </ul>
-        <Loader/>
-        <LoadButton func={() => {setFetching(true)}}/>     
+      <title>Cards</title>
+      <ul className="CardsWrapper">
+        {!!cards.length &&
+          cards.map((card) => {
+            return (
+              <Card
+                key={card.id}
+                url={card.url}
+                alt={card.title}
+                id={card.id}
+              />
+            );
+          })}
+      </ul>
+      <Loader />
+      <LoadButton
+        func={() => {
+          setFetching(true);
+        }}
+      />
     </>
-    
   );
-}
+};
 
-export default Home
+export default Home;
